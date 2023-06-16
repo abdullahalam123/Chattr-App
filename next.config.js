58cf34f97bdad5c5978cca5938605ca3c1effd64
abdullahalam123 <abdullah.alam.tapadar2000@gmail.com>
2023-06-16 18:01:52 +0530

    added appdir

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  experimental: {
+    appDir: true,
+    // swcPlugins: [["next-superjson-plugin", {}]],
+  },
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
